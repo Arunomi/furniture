13807a1e8b12e4beac593a8969013bd5c2fbc4b3
Arunomi <vypirovsky@gmail.com>
2021-02-10 18:50:14 +0200

    saving all data to git. Header and kitchen gallery ready

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+
+$(function () {
+  
+    $('.bg__slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        // fade: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        speed: 800
+    });
+
+});
